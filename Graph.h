37ca5761fb777dde08dbcd414704155754efef77
tommygomez25 <maysan54321@gmail.com>
2022-01-22 17:57:52 +0000

    adicionei minHeap e toni djiksta

--- Graph.h
+++ Graph.h
@@ -5,14 +5,15 @@
 #include <list>
 #include <iostream>
 #include <queue>
+#include "minHeap.h"
 
 using namespace std;
 
-class Graph {
+class graph {
 public:
     struct Edge {
         int dest;   // Destination node
-        int weight; // An integer weight
+        double weight; // An integer weight
         string line; //
     };
 
@@ -28,27 +29,31 @@ public:
         float longitude;
     };
 
-    int n;              // Graph size (vertices are numbered from 1 to n)
+    int n;              // graph size (vertices are numbered from 1 to n)
     bool hasDir;        // false: undirect; true: directed
-     // The list of nodes being represented
+    // The list of nodes being represented
 
-    void dijkstra(int s);
+    void dijkstra(int s, int r);
 
 
     vector<Node> nodes;
     // Constructor: nr nodes and direction (default: undirected)
-    Graph(int nodes, bool dir = false);
+    graph(int nodes, bool dir = false);
 
     // Add edge from source to destination with a certain weight
-    void addEdge(int src, int dest, string line,int weight = 1);
+    void addEdge(int src, int dest, string line,double weight = 1);
 
     // ----- Functions to implement in this class -----
     int dijkstra_distance(int a, int b);
     list<int> dijkstra_path(int a, int b);
     vector<Node> getNodes(){return nodes;}
-    void bfs(int v, int b);
+    int bfs(int v, int b);
 };
 
+
+
+
+#endif //PROJETOAED2_GRAPH_H
 
 
 
